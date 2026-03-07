ba85657886ac7065964df6ae0c799b243cca5c4a
dhiafersi <dhiafersi@example.com>
2026-03-07 16:42:29 +0100

    Allow remote images from any HTTPS host

--- next.config.ts
+++ next.config.ts
@@ -5,23 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'example.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i5.walmartimages.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gamesir.com',
+        hostname: '**',
       }
     ],
   },
